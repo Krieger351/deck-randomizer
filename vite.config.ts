f4b72c4922eb3d9591d3bca8ce0a95b25b4a233d
Alex Glica <alexander.glica@gmail.com>
2021-09-14 15:21:35 -0400

    Minify output

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
+import { minifyHtml } from "vite-plugin-html";
 
 export default defineConfig({
   build: {
     outDir: "docs",
   },
+  plugins: [minifyHtml()],
   server: {
     open: true,
   },
